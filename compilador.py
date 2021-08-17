255c6c8c7b8469d914467f83e7ae0f58e3609d96
ThiagoMelo2000 <thiaguinho.melo2000@gmail.com>
2021-08-16 23:04:47 -0300

    v0.0.2-feature-ADT

--- compilador.py
+++ compilador.py
@@ -1,72 +1,78 @@
-def separador(linha):
-
-    tipo_variavel = ['int', 'float', 'char']
-    reservada_print = ['printf']
-    simbolo_fim = [';']
-    simbolo = ['=']
-
-    # Declaração de Variável
-    if(linha[0] in tipo_variavel):
-        
-        if(linha[1] not in tipo_variavel and linha[1] not in simbolo_fim):
-            
-            if(linha[2] in simbolo_fim):
-                
-                return 'v_declarado'
+def declaracao(linha):
     
-    # Atribuição de Variável
+    reservada = ['int', 'float', 'char']
+    simbolos = ['.', ',', '=', '!', '(', ')', '%', '*', '+', '-', '/', '<', '>', ' ']
+    
+    if(linha[0] in reservada):
+        
+        if(linha[2] == ';'):
+            
+            if(linha[1] not in reservada and linha[1] not in simbolos):
+                
+                variaveis.append(linha[1])
+        
+
+def atribuicao(linha):
+    
+    reservada = ['int', 'float', 'char']
+    simbolos = ['.', ',', '=', '!', '(', ')', '%', '*', '+', '-', '/', '<', '>', ' ']
+    operadores = ['+', '-', '*', '/']
+    tamanho = len(linha)
+    controle = 0
+    
     if(linha[0] in variaveis):
         
-        if(linha[1] == '='):
-            
-            if(linha[2] not in tipo_variavel and linha[2] not in simbolo):
-                
-                if(linha[3] in simbolo_fim):
-                    
-                    valores.append(linha[2])
-                    
-    if(linha[0] in reservada_print):
+        if(linha[tamanho-1] == ';'):
         
-        if(linha[2] in variaveis):
-            
-            pos = variaveis.index(linha[2])
-            
-            if(linha[4] in simbolo_fim):
+            if(linha[1] == '='):
                 
-                return valores[pos]
-   
+                controle = 1
+                
+    if(controle == 1):
+        
+        for i in range(2, tamanho-1):
+                    
+            if(i % 2 == 0):
+                
+                if(linha[i] not in reservada and linha[i] not in simbolos):
+                    
+                    # print('Variavel')
+                    # -----------------------------------------------------------
+                    # Fazer a conversão para outra linguagem
+                    # -----------------------------------------------------------
+                    ...
+                    
+                else:
+                    
+                    print('Erro')
+                
+            elif(i % 2 != 0):
+                
+                if(linha[i] in operadores):
+                    
+                    # print('2')
+                    # -----------------------------------------------------------
+                    # Fazer a conversão para outra linguagem
+                    # -----------------------------------------------------------
+                    ...
+                    
+                else:
+                    
+                    print('Erro')
+            
+            # print(i)
 
 
 texto = open('file.txt', 'r')
 linhas = texto.readlines()
 
-resultado = 0
-
 global variaveis
-global valores
-
 variaveis = list()
-valores = list()
 
 for linha in linhas:
     
     linha = linha.split()
+    declaracao(linha)
+    atribuicao(linha)
     
-    # print(linha)
-    retorno = separador(linha)
-    # print(retorno)
-    
-    if(retorno != None and retorno != 'v_declarado'):
-        
-        print(retorno)
-    
-    
-    if(retorno == 'v_declarado'):
-                
-        variaveis.append(linha[1])
-        
-    # valores.append(retorno)
-    
-# print(valores)
-# print(variaveis)
-    
+print(variaveis)
